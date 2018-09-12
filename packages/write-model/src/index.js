501bbd4f2e41fe3e8b4573777e81875836746f99
Yonah Forst <yonaforst@hotmail.com>
2018-09-12 23:45:48 +0200

    added missing index

---
+++ packages/write-model/src/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  repositoryBuilder: require('./repositoryBuilder'),
+  commandServiceBuilder: require('./commandServiceBuilder'),
+  serverlessHandlerBuilder: require('./serverlessHandlerBuilder'),
+}
