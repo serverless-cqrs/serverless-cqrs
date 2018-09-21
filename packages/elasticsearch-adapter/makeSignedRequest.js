8b46f3d5e4fc5395bd194e7df5c2d64cfd355c46
Yonah Forst <yonaforst@hotmail.com>
2018-09-21 19:41:54 +0200

    fix signed request

--- packages/elasticsearch-adapter/makeSignedRequest.js
+++ packages/elasticsearch-adapter/makeSignedRequest.js
@@ -2,12 +2,12 @@ const AWS = require('aws-sdk');
 const axios = require('axios');
 
 module.exports = ({ endpoint, path, body, service, method, region }) => {
-		const url = endpoint + path
-		let req = new AWS.HttpRequest(url, region);
+	const url = endpoint + path
+	let req = new AWS.HttpRequest(url, region);
     req.method = method
     req.headers.host = req.endpoint.host
     req.headers['Content-Type'] = 'application/json';
-    req.body = body
+    req.body = JSON.stringify(body)
 
     let signer = new AWS.Signers.V4(req, service, true);
     signer.addAuthorization(AWS.config.credentials, AWS.util.date.getDate());
