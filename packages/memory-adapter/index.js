68f6a438b3286bd5e3a78cd6319a54f3322b08da
Yonah Forst <yonaforst@hotmail.com>
2018-12-23 10:54:03 +0100

    store events already normalized

--- packages/memory-adapter/index.js
+++ packages/memory-adapter/index.js
@@ -1,8 +1,17 @@
+
 module.exports.build = ({ entityName }, { eventStore={}, projectionStore={} }={}) => {
   const eventClient = eventStore[entityName] || (eventStore[entityName] = [])
   const projClient = projectionStore[entityName] || (projectionStore[entityName] = {})
+  const listeners = {}
+  const notifyListeners = commit => Object.values(listeners).map(func => func(commit))
 
   return {
+    addListener: (id, func) => {
+      listeners[id] = func
+    },
+    removeListener: id => {
+      delete listeners[id]
+    },
     parseCommit: c => c,
     loadEvents: async (entityId, version=0) => {
       const res = eventClient.filter(e => e.entityId == entityId && e.version > version)
@@ -13,19 +22,21 @@ module.exports.build = ({ entityName }, { eventStore={}, projectionStore={} }={}
       const res = eventClient.filter(e => e.commitId > commitId)
       return res
     },
-    append: async (entityId, version, events) => {
+    append: async (id, version, events) => {
       const committedAt = Date.now()
       const commitId = committedAt.toString() + Math.random().toString(36).slice(2, 8)
 
-      eventClient.push({
+      const commit = {
+        id,
+        events,
         commitId,
         committedAt,
-        entityId,
-        entityName,
-        version,
-        events,
-      })
-
+        entity: entityName,
+        version: parseInt(version),
+      }
+    
+      eventClient.push(commit)
+      notifyListeners(commit)
       return true
     },
     set: async (id, { version, state }) => {
