bf3e3f65c5570e9271527c12a2a89d371e949964
Yonah Forst <yonaforst@hotmail.com>
2018-12-26 12:16:41 +0100

    use native http instead of axios

--- packages/elasticsearch-adapter/makeSignedRequest.js
+++ packages/elasticsearch-adapter/makeSignedRequest.js
@@ -1,5 +1,12 @@
 const AWS = require('aws-sdk');
-const axios = require('axios');
+
+const parseJson = text => {
+	try {
+		return JSON.parse(text)
+	} catch (e) {
+		return text
+	}
+}
 
 module.exports = ({ endpoint, path, body, service, method, region }) => {
 	const url = endpoint + path
@@ -12,10 +19,26 @@ module.exports = ({ endpoint, path, body, service, method, region }) => {
     let signer = new AWS.Signers.V4(req, service, true);
     signer.addAuthorization(AWS.config.credentials, AWS.util.date.getDate());
 
-    return axios({
-        method,
-        url,
-        data: req.body,
-        headers: req.headers
-    });
+
+    var client = new AWS.HttpClient();
+    return new Promise((resolve, reject) => {
+			client.handleRequest(req, null, res => {
+				var body = ''
+				res.on('data', chunk => body += chunk)
+				res.on('end', () => {
+
+					
+					const response = {
+						body,
+						status: res.statusCode,
+						statusMessage: res.statusMessage,
+						data: parseJson(body),
+					}
+
+					res.statusCode >= 200 && res.statusCode < 300
+						? resolve(response)
+						: reject({ response })	
+				})
+			}, reject)
+    })
 }
